577bb5edcc3754bdb2effd81965417b920c7a656
ErikMZ <sistemas@larc.pe>
2023-07-30 13:51:02 -0500

    Revert "creando funcion crearHome"
    This reverts commit 41765a1a98b664590962cf5590cac4d4603cfd82.

--- index.js
+++ index.js
@@ -12,15 +12,6 @@ function Home() {
         return variableA*3 + variableB*variableC
     }
 }
-function crearHome() {
-    const listItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    const addItem = () => {
-        const variableA = 1;
-        const variableB = 3;
-        const variableC = 2;
-        return variableA*3 + variableB*variableC
-    }
-}
 
 return(
     <><div>Home</div>
