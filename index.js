adb787bf5179d546b259aa31123c5b0a928ee335
ErikMZ <sistemas@larc.pe>
2023-07-30 13:22:47 -0500

    Creando funcion en index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Home() {
+    const listItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const addItem = () => {
+        const variableA = 1;
+        const variableB = 3;
+        const variableC = 2;
+        return variableA*3 + variableB*variableC
+    }
+}
+
+return(
+    <><div>Home</div>
+    <button onclick={addItem}></button>
+    </>
+)
+
+export default Home
